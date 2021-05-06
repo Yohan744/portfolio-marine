2a99eb31dee21aee9848b04f9a0ca4575a89b3a1
Yohan744 <73723399+Yohan744@users.noreply.github.com>
2021-05-07 00:43:34 +0200

    fix animation

--- public/js/home.js
+++ public/js/home.js
@@ -16,7 +16,7 @@ new fullpage("#fullpage", {
 })
 
 /*---------------------------------------Loading screen-----------------------------------------*/
-/*
+
 const loadingScreen = document.querySelector("#loadingScreen")
 let loadingVerif = false
 
@@ -33,7 +33,7 @@ window.addEventListener("load", () => {
             }, 1);
         }, 14000);
     }
-})*/
+})
 
 /*-------------------------------------------Navigation---------------------------------------------*/
 
